7746b0188f5330969205f90b96636bb264091911
Marcos Monteiro <mrcsmonteiro@gmail.com>
2023-06-13 18:10:59 +1000

    Add location parameter to curl command

--- labfiles/debugging/prom-init.sh
+++ labfiles/debugging/prom-init.sh
@@ -9,7 +9,7 @@ $SUDO useradd --no-create-home --shell /bin/false prometheus
 $SUDO mkdir /etc/prometheus
 $SUDO mkdir /var/lib/prometheus
 $SUDO chown prometheus:prometheus /var/lib/prometheus
-(cd /tmp/; curl https://github.com/prometheus/prometheus/releases/download/v2.20.0/prometheus-2.20.0.linux-amd64.tar.gz -O)
+(cd /tmp/; curl -LO https://github.com/prometheus/prometheus/releases/download/v2.20.0/prometheus-2.20.0.linux-amd64.tar.gz)
 (cd /tmp/; tar -xvf prometheus-2.20.0.linux-amd64.tar.gz)
 $SUDO mv /tmp/prometheus-2.20.0.linux-amd64/console* /etc/prometheus
 $SUDO mv /tmp/prometheus-2.20.0.linux-amd64/prometheus.yml /etc/prometheus
