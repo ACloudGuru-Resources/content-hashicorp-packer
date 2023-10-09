c8535c1dc541d3584cdb321e3fc69b1f6e67b57c
Marcos Monteiro <mrcsmonteiro@gmail.com>
2023-10-09 13:31:51 +1100

    Add command to reload apache2

--- labfiles/bash/webdeploy.sh
+++ labfiles/bash/webdeploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 git clone https://github.com/ACloudGuru-Resources/content-hashicorp-packer.git /tmp/files
 sudo mkdir -p /var/www/html/bookbinge
 sudo mv /tmp/files/sample-website/* /var/www/html/bookbinge/
@@ -8,3 +7,4 @@ sudo mv /tmp/files/labfiles/bash/bookbinge.conf /etc/apache2/sites-available/
 sudo a2ensite bookbinge.conf
 sudo systemctl reload apache2
 sudo rm -rf /tmp/files
+sudo systemctl reload apache2
